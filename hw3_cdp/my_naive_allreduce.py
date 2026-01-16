df5c5063e8c4d9e3c36adfcb8c2a13f03fc82b0a
Ofek <ofek.israel@campus.technion.ac.il>
2026-01-16 10:18:28 +0200

    Part 1 (not tested)

--- hw3_cdp/my_naive_allreduce.py
+++ hw3_cdp/my_naive_allreduce.py
@@ -1,4 +1,5 @@
 import numpy as np
+import mpi4py.MPI as MPI
 
 
 def allreduce(send, recv, comm, op):
@@ -13,4 +14,19 @@ def allreduce(send, recv, comm, op):
     comm : MPI.Comm
     op : associative commutative binary operator
     """
-    raise NotImplementedError("To be implemented")
+    size = comm.Get_size()
+    rank = comm.Get_rank()
+
+    for i in range(size):
+        if i == rank:
+            pass
+        else:
+            comm.Send(send, dest=i)
+
+    for i in range(size):
+        if i == rank:
+            pass
+        else:
+            temp = np.empty_like(send)
+            comm.Recv(temp, source=i)
+            recv = op(recv, temp)
--- hw3_cdp/my_ring_allreduce.py
+++ hw3_cdp/my_ring_allreduce.py
@@ -1,4 +1,5 @@
 import numpy as np
+import mpi4py.MPI as MPI
 
 
 def ringallreduce(send, recv, comm, op):
@@ -14,4 +15,19 @@ def ringallreduce(send, recv, comm, op):
     comm : MPI.Comm
     op : associative commutative binary operator
     """
-    raise NotImplementedError("To be implemented")
+    size = comm.Get_size()
+    rank = comm.Get_rank()
+    blockSize = send.shape[0] // size
+
+    for _ in range(2):
+        for i in range(size):
+            end = (i + 1) * blockSize
+            if i == size - 1:
+                end = send.shape[0]
+            comm.Send(send[i * blockSize: end], dest=(rank + 1) % size)
+            temp = np.empty_like(send[i * blockSize: end])
+            comm.Recv(temp, source=(rank - 1) % size)
+            endR = i * blockSize
+            if i == 0:
+                endR = send.shape[0]
+            recv[((i - 1 + size) % size) * blockSize: endR] = op(recv[((i - 1 + size) % size) * blockSize: endR], temp)
